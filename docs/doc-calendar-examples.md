a8d5934c56f652a762d0e6335be07e822c360998
Patrick Hensley <pathensley@gmail.com>
2018-07-03 19:52:08 -0400

    Date formatting examples.

---
+++ docs/doc-calendar-examples.md
@@ -0,0 +1,16 @@
+---
+id: doc-calendar-examples
+title: Examples
+---
+
+All examples below use:
+
+```typescript
+// UTC
+const date = new Date(2018, 2, 15, 12, 34, 56, 789);
+
+// Formats that include time will use these 3 zones
+const zones = [ 'America/New_York', 'Europe/London', 'Asia/Tokyo' ];
+```
+
+{> output/doc-calendar-examples.md }
---
+++ website/examples/doc-calendar-examples.ts
@@ -0,0 +1,106 @@
+import { inspect } from 'util';
+import { framework } from './helpers';
+import { FormatWidthType } from '@phensley/cldr';
+
+const ts = (s: string) => '```typescript\n' + s + '\n```';
+const html = (s: string) => '<pre class="output">\n' + s + '\n</pre>';
+const method = (o: any) => `cldr.Calendars.formatDate(date, ${inspect(o)});`;
+const header = (s: string) => console.log(`## ${s}\n\n`);
+
+// formatDate
+(() => {
+  const cldr = framework.get('en');
+  const epoch = new Date(2018, 2, 15, 12, 34, 56, 789);
+  const newyork = 'America/New_York';
+  const tokyo = 'Asia/Tokyo';
+  const london = 'Europe/London';
+
+  const zones = [newyork, london, tokyo];
+  const widths: FormatWidthType[] = ['short', 'medium', 'long', 'full'];
+
+  header('Dates');
+
+  for (const width of widths) {
+    const opts = { date: width };
+    const d = { epoch, zoneId: newyork };
+    console.log(ts(method(opts)));
+    const res = cldr.Calendars.formatDate(d, opts);
+    console.log(html(res));
+    console.log();
+  }
+
+  header('Times');
+
+  for (const width of widths) {
+    const opts = { time: width };
+    console.log(ts(method(opts)));
+
+    let res: string[] = [];
+    for (const zoneId of zones) {
+      const d = { epoch, zoneId };
+      const s = cldr.Calendars.formatDate(d, opts);
+      res.push(s);
+    }
+    console.log(html(res.join('\n')));
+    console.log();
+  }
+
+  header('Dates and times');
+
+  for (const width of widths) {
+    const opts = { datetime: width };
+    console.log(ts(method(opts)));
+
+    let res: string[] = [];
+    for (const zoneId of zones) {
+      const d = { epoch, zoneId };
+      const s = cldr.Calendars.formatDate(d, opts);
+      res.push(s);
+    }
+    console.log(html(res.join('\n')));
+    console.log();
+  }
+
+  header('Date skeletons');
+
+  // Skeleton examples
+
+  let skeletons = ['yMd', 'yMMMd', 'yMMMMd', 'EEEEyMMMMd'];
+  for (const skeleton of skeletons) {
+    const opts = { skeleton };
+    console.log(ts(`cldr.Calendars.formatDate(date, ${inspect(opts)});`));
+    const res = cldr.Calendars.formatDate({ epoch, zoneId: newyork }, opts);
+    console.log(html(res));
+    console.log();
+  }
+
+  header('Time skeletons');
+
+  skeletons = ['hm', 'hmsz', 'hmszzzz', 'hmsVVVV', 'hmsvvvv'];
+  for (const skeleton of skeletons) {
+    const opts = { skeleton };
+    console.log(ts(`cldr.Calendars.formatDate(date, ${inspect(opts)});`));
+    let res: string[] = [];
+    for (const zoneId of zones) {
+      const s = cldr.Calendars.formatDate({ epoch, zoneId }, opts);
+      res.push(s);
+    }
+    console.log(html(res.join('\n')));
+    console.log();
+  }
+
+  header('Date time skeletons');
+
+  skeletons = ['yMdhm', 'yMMMdhmsz', 'EyMMMMdhmszzzz', 'yMMMdhmsVVVV', 'yMMMMdhmsvvvv'];
+  for (const skeleton of skeletons) {
+    const opts = { skeleton };
+    console.log(ts(`cldr.Calendars.formatDate(date, ${inspect(opts)});`));
+    let res: string[] = [];
+    for (const zoneId of zones) {
+      const s = cldr.Calendars.formatDate({ epoch, zoneId }, opts);
+      res.push(s);
+    }
+    console.log(html(res.join('\n')));
+    console.log();
+  }
+})();
