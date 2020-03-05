68f83f16e50ed472d9ca596fe6b2567f615805be
Patrick Hensley <pathensley@gmail.com>
2020-03-05 13:10:22 -0500

    Add link to yarn package

--- website/src/data.js
+++ website/src/data.js
@@ -3,8 +3,9 @@ module.exports = {
     { doc: 'doc-index', label: 'Docs' },
     { doc: 'api-cldr', label: 'API' },
     // { href: '/cldr-engine/liveapi.html', label: 'Live'}, // TODO: restore when ready
-    { href: 'https://phensley.github.io/cldr-engine-react-demo', label: 'Demo '},
+    { href: 'https://phensley.github.io/cldr-engine-react-demo', label: 'Demo ' },
     { href: 'https://github.com/phensley/cldr-engine', label: 'Github' },
+    { href: 'https://yarnpkg.com/package/@phensley/cldr', label: 'Yarn' },
     { href: 'https://www.npmjs.com/package/@phensley/cldr', label: 'NPM' }
   ]
 };
